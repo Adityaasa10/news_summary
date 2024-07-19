cfe82bf3bdf647a13085a8f931d860be3ebe37f1
Adityaasa10 <adityaasa10@gmail.com>
2024-07-19 11:00:34 +0530

    new commit

--- summarizer.py
+++ summarizer.py
@@ -245,4 +245,6 @@ def main():
     return redirect("/") 
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
